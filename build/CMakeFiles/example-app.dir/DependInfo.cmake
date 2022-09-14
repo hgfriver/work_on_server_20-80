6584aa667ed37d9c193c21e548e84525e25d3d6c
river <hgfriver@gmail.com>
2022-09-15 02:36:10 +0800

    first commit

---
+++ build/CMakeFiles/example-app.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/river/Documents/torch_on_device_20-80/example-app.cpp" "/home/river/Documents/torch_on_device_20-80/build/CMakeFiles/example-app.dir/example-app.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_C10D_GLOO"
+  "USE_DISTRIBUTED"
+  "USE_RPC"
+  "USE_TENSORPIPE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/river/libtorch-shared-with-deps-1.12.1+cpu/libtorch/include"
+  "/home/river/libtorch-shared-with-deps-1.12.1+cpu/libtorch/include/torch/csrc/api/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
